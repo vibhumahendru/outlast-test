638cc6b1ad689b9cc7218f4a4b21a02a34c582e5
vibhumahendru <vibhumahendru@gmail.com>
2022-01-24 20:22:45 +0530

    test

--- outlast_backend/app.js
+++ outlast_backend/app.js
@@ -6,15 +6,27 @@ const cors = require('cors')
 const PORT = 8080;
 
 app.use(express.json())
-app.use(cors())
+app.use(cors()) 
 
-app.listen(PORT, () => console.log("server runninng"));
+app.listen(PORT, () => console.log("server running"));
 
 app.get("/get-favorites", (req, resp) => {
   let data = [];
-  axios.get("http://jsonbase.com/vibhu/books").then((res) => {
+  let userId = req.body['user_id']
+
+  axios.get(`http://jsonbase.com/${userId}/books`).then((res) => {
     data = res.data;
     resp.status(200).send(data);
+  });
+});
+
+app.post("/set-favorites", (req, resp) => {
+  let payload = req.body;
+  let userId = req.body['user_id']
+
+  axios.put(`http://jsonbase.com/${userId}/books`, payload).then((res) => {
+    data = res.data;
+    resp.status(201).send(data);
   });
 });
 
